b96805e2e350cfbce23f8c2798dda87fa4b2af3e
hmarinoo <hmarinoo@gmail.com>
2019-02-20 23:20:53 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,16 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Date;
 
-import db.DB;
-import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1,"Books");
-		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "rolo", "rolo@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(seller);
 		
 	}
 
