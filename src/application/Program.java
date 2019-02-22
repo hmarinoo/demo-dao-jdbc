f19afb610ef754b4fa9ac1638ab500d6a7935178
hmarinoo <hmarinoo@gmail.com>
2019-02-22 00:07:20 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,8 +18,12 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller );
 		
-		System.out.println("\n=== Test 1: seller findByDepartment" );
+		System.out.println("\n=== Test 2: seller findByDepartment" );
 		List<Seller> sellers = sellerDao.findByDepartment(new Department(2, null));
+		sellers.forEach(System.out::println);
+		
+		System.out.println("\n=== Test 3: seller findAll" );
+		sellers = sellerDao.findAll();
 		sellers.forEach(System.out::println);
 	}
 
